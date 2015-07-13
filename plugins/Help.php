0276d68b8ee7e6b5e3dff46904a3dd63c9c60365
dray <hnzksq@gmail.com>
2015-07-13 22:54:29 +0800

    删除多余的代码

--- plugins/Help.php
+++ plugins/Help.php
@@ -20,42 +20,6 @@ class Help extends Base
     }
 
     /**
-     * 当有人进入群的时候
-     */
-    public function msg_enter_chat() {
-        CFun::echo_log("有人进入群");
-
-//        $text = '欢迎 ' . $this->new_name . ' 来到这里！ ' . PHP_EOL;
-//        $text .= 'Welcome ' . $this->new_name . ' to here！ ' . PHP_EOL;
-//
-//        $msg = Telegram::singleton()->post('sendMessage', array(
-//            'chat_id'             => $this->chat_id,
-//            'text'                => $text,
-//            'reply_to_message_id' => $this->msg_id,
-//        ));
-//
-//        CFun::echo_log("发送信息: msg=%s", $msg);
-    }
-
-    /**
-     * 有人离开群的时候
-     */
-    public function msg_left_chat() {
-        CFun::echo_log("有人离开群");
-
-//        $text = '欢送 ' . $this->level_name . ' 离开这里！ ' . PHP_EOL;
-//        $text .= 'Farewell ' . $this->level_name . ' out of here！ ' . PHP_EOL;
-//
-//        $msg = Telegram::singleton()->post('sendMessage', array(
-//            'chat_id'             => $this->chat_id,
-//            'text'                => $text,
-//            'reply_to_message_id' => $this->msg_id,
-//        ));
-//
-//        CFun::echo_log("发送信息: msg=%s", $msg);
-    }
-
-    /**
      * 得到说明信息
      * 如果有参数，那么就拿一个的，否则拿取所有的
      * @param null $text
@@ -65,6 +29,8 @@ class Help extends Base
         $helps   = array();
         $router  = CFun::get_router();
         $plugins = array_flip($router);
+
+        $one = false;
         foreach ($plugins as $class_name => $tmp) {
             $class = Process::get_class($class_name);
 
@@ -74,7 +40,9 @@ class Help extends Base
                 if (!is_array($desc)) {
                     $desc = array($desc);
                 }
+
                 $helps = $desc;
+                $one   = true;
                 break;
             }
 
@@ -92,11 +60,13 @@ class Help extends Base
             $helps = array_merge($helps, $desc);
         }
 
-        $helps = array_merge($helps, array(
-            '',
-            'GitHub: https://github.com/DrayChou/tgbot-php',
-            'Author: @drayc',
-        ));
+        if (false == $one) {
+            $helps = array_merge($helps, array(
+                '',
+                'GitHub: https://github.com/DrayChou/tgbot-php',
+                'Author: @drayc',
+            ));
+        }
 
         return implode(PHP_EOL, $helps);
     }
