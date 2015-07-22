f2935d2bf150f4f261540a5070de95610cd66698
dray <hnzksq@gmail.com>
2015-07-22 18:38:45 +0800

    添加调试代码

--- public/index.php
+++ public/index.php
@@ -21,16 +21,28 @@ require_once(LIB_PATH . 'process.php');
 //设置时区
 date_default_timezone_set(CFun::get_config('timezone', 'Asia/Shanghai'));
 
-//死循环查询
-do {
-    CFun::G('run_start');
+if (empty($_REQUEST)) {
+    phpinfo();
+} else {
 
-    Process::run();
+    //发调试信息
+    $admins = CFun::get_config('admins');
+    Telegram::singleton()->send_message(array(
+        'chat_id' => $admins[0],
+        'text'    => json_encode($_REQUEST),
+    ));
 
-    CFun::G('run_end');
-    $use_time = CFun::G('run_start', 'run_end');
-    $use_mem  = CFun::G('run_start', 'run_end', 'm');
-    echo '耗时：' . $use_time . ' 耗内存：' . $use_mem . PHP_EOL . ' 当前占内存：' . CFun::convert_memory_size(memory_get_usage()) . PHP_EOL;
+    //死循环查询
+    do {
+        CFun::G('run_start');
 
-    usleep(INTERVAL);// 等待
-} while (true);
+        Process::run();
+
+        CFun::G('run_end');
+        $use_time = CFun::G('run_start', 'run_end');
+        $use_mem  = CFun::G('run_start', 'run_end', 'm');
+        echo '耗时：' . $use_time . ' 耗内存：' . $use_mem . PHP_EOL . ' 当前占内存：' . CFun::convert_memory_size(memory_get_usage()) . PHP_EOL;
+
+        usleep(INTERVAL);// 等待
+    } while (true);
+}
