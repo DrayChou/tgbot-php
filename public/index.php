b94b00fc11f6023fee475397770e46544e0c1ec3
dray <hnzksq@gmail.com>
2015-07-23 12:45:45 +0800

    调整调试代码

--- public/index.php
+++ public/index.php
@@ -18,18 +18,8 @@ date_default_timezone_set(CFun::get_config('timezone', 'Asia/Shanghai'));
 
 //如果有 token 带过来，那么调用对应的机器人
 if(isset($_GET['token'])){
+    // 设置 token
     CFun::set_config('token', $_GET['token']);
-
-    //发调试信息
-    $admins = CFun::get_config('admins');
-    Telegram::singleton()->send_message(array(
-        'chat_id' => -25936895,
-        'text'    => json_encode(array(
-            '$_GET' => $_GET,
-            '$_POST' => $_POST,
-            'input' => file_get_contents('php://input'),
-        )),
-    ));
 
     CFun::G('run_start');
 
@@ -42,7 +32,17 @@ if(isset($_GET['token'])){
     $use_mem  = CFun::G('run_start', 'run_end', 'm');
     $log = '耗时：' . $use_time . ' 耗内存：' . $use_mem . PHP_EOL . ' 当前占内存：' . CFun::convert_memory_size(memory_get_usage()) . PHP_EOL;
 
-    CFun::echo_log($log);
+    //发调试信息
+    $admins = CFun::get_config('admins');
+    Telegram::singleton()->send_message(array(
+        'chat_id' => -25936895,
+        'text'    => json_encode(array(
+            '$_GET' => $_GET,
+            '$_POST' => $_POST,
+            'input' => file_get_contents('php://input'),
+            'log' => $log,
+        )),
+    ));
 }else{
     echo 'test' . PHP_EOL;
 }
