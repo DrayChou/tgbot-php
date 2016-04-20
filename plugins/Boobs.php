b91e1503866a91903f31fd067cea4850a4936d45
dray <hnzksq@gmail.com>
2016-04-20 23:27:37 +0800

    添加复数的图片参数

--- plugins/Boobs.php
+++ plugins/Boobs.php
@@ -8,11 +8,13 @@
  */
 class Boobs extends Base
 {
-    static function desc() {
+    public static function desc()
+    {
         return "/boobs - Get a boobs NSFW image. ";
     }
 
-    static function usage() {
+    public static function usage()
+    {
         return array(
             "/boobs - Get a boobs NSFW image. ",
         );
@@ -21,23 +23,44 @@ class Boobs extends Base
     /**
      * 当命令满足的时候，执行的基础执行函数
      */
-    public function run() {
+    public function run()
+    {
         Common::echo_log("执行 Boobs run");
 
-        $url = "http://api.oboobs.ru/noise/1";
+        //图片数量
+        $send_image_num = 1;
+        if (count($this->parms) == 2) {
+            if (is_numeric($this->parms[1])) {
+                $send_image_num = $this->parms[1];
+            }
+        }
+
+        $url = "http://api.oboobs.ru/noise/{$send_image_num}";
         $res = Common::curl($url);
 
+        $res_str = null;
         if (!isset($res) || !isset($res[0]['preview'])) {
             $res_str = 'Cannot get that boobs, trying another one...';
         } else {
-            $res_str = 'http://media.oboobs.ru/' . $res[0]['preview'];
+            foreach ($res as $v) {
+                $res_str = 'http://media.oboobs.ru/' . $v['preview'];
+                //回复消息
+                Telegram::singleton()->send_message(array(
+                    'chat_id' => $this->from_id,
+                    'text' => $res_str,
+                    // 'reply_to_message_id' => $this->msg_id,
+                ));
+            }
         }
 
-        //回复消息
-        Telegram::singleton()->send_message(array(
-            'chat_id'             => $this->from_id,
-            'text'                => $res_str,
-            // 'reply_to_message_id' => $this->msg_id,
-        ));
+        if (empty($res_str)) {
+            $res_str = 'Cannot get that boobs, trying another one...';
+            //回复消息
+            Telegram::singleton()->send_message(array(
+                'chat_id' => $this->from_id,
+                'text' => $res_str,
+                // 'reply_to_message_id' => $this->msg_id,
+            ));
+        }
     }
 }
--- plugins/Butts.php
+++ plugins/Butts.php
@@ -8,11 +8,13 @@
  */
 class Butts extends Base
 {
-    static function desc() {
+    public static function desc()
+    {
         return "/butts - Get a butts NSFW image. ";
     }
 
-    static function usage() {
+    public static function usage()
+    {
         return array(
             "/butts - Get a butts NSFW image. ",
         );
@@ -21,23 +23,44 @@ class Butts extends Base
     /**
      * 当命令满足的时候，执行的基础执行函数
      */
-    public function run() {
+    public function run()
+    {
         Common::echo_log("执行 Butts run");
 
-        $url = "http://api.obutts.ru/noise/1";
-        $res = Common::curl($url);
-
-        if (!isset($res) || !isset($res[0]['preview'])) {
-            $res_str = 'Cannot get that butts, trying another one...';
-        } else {
-            $res_str = 'http://media.obutts.ru/' . $res[0]['preview'];
+        //图片数量
+        $send_image_num = 1;
+        if (count($this->parms) == 2) {
+            if (is_numeric($this->parms[1])) {
+                $send_image_num = $this->parms[1];
+            }
         }
 
-        //回复消息
-        Telegram::singleton()->send_message(array(
-            'chat_id'             => $this->from_id,
-            'text'                => $res_str,
-            // 'reply_to_message_id' => $this->msg_id,
-        ));
+        $url = "http://api.obutts.ru/noise/{$send_image_num}";
+        $res = Common::curl($url);
+
+        $res_str = null;
+        if (!isset($res) || !isset($res[0]['preview'])) {
+            $res_str = 'Cannot get that boobs, trying another one...';
+        } else {
+            foreach ($res as $v) {
+                $res_str = 'http://media.obutts.ru/' . $v['preview'];
+                //回复消息
+                Telegram::singleton()->send_message(array(
+                    'chat_id' => $this->from_id,
+                    'text' => $res_str,
+                    // 'reply_to_message_id' => $this->msg_id,
+                ));
+            }
+        }
+
+        if (empty($res_str)) {
+            $res_str = 'Cannot get that boobs, trying another one...';
+            //回复消息
+            Telegram::singleton()->send_message(array(
+                'chat_id' => $this->from_id,
+                'text' => $res_str,
+                // 'reply_to_message_id' => $this->msg_id,
+            ));
+        }
     }
 }
