82e58fc2e39e0bd2f1bb2895eed2a2da9dbecc8b
dray <hnzksq@gmail.com>
2016-05-15 17:39:14 +0800

    1.添加查看管理员的命令

--- plugins/Admin.php
+++ plugins/Admin.php
@@ -4,7 +4,7 @@
  * @Author: dray
  * @Date:   2016-05-15 11:43:42
  * @Last Modified by:   dray
- * @Last Modified time: 2016-05-15 16:46:29
+ * @Last Modified time: 2016-05-15 17:38:21
  */
 
 class Admin extends Base
@@ -66,13 +66,6 @@ class Admin extends Base
             return true;
         }
 
-        //检查参数
-        $tmp = explode(' ', $this->text, 2);
-        if (count($tmp) != 2) {
-            Common::echo_log("Admin: 参数不足，退出");
-            return true;
-        }
-
         $bot = Db::get_bot_name();
         $redis = Db::get_redis();
 
@@ -80,23 +73,32 @@ class Admin extends Base
         $admins_key = $bot . "chat:{$this->chat_id}:admins";
 
         //检查权限
-        $big_admins = Common::get_config('admins');
+        $sup_admins = Common::get_config('admins');
         $chat_admins = $redis->hGetAll($admins_key);
-        $all_admins = array_merge($big_admins, array_keys($chat_admins));
+        $all_admins = array_merge($sup_admins, array_keys($chat_admins));
         if (!in_array($this->from_id, $all_admins)) {
-            Common::echo_log("Admin: 不是管理员，退出");
+            Common::echo_log("Admin: 不是管理员，退出" . print_r($sup_admins, true) . print_r($chat_admins, true));
             return;
         }
 
+        //检查参数
+        $tmp = explode(' ', $this->text, 2);
         $do = $tmp[0];
-        $value = $tmp[1];
+
         $res_str = '';
 
-        Common::echo_log("Admin: do:{$do} $value:{$value}");
+        Common::echo_log("Admin: do:{$do}");
 
         switch ($do) {
             //添加管理员
             case 'add':{
+                    if (count($tmp) != 2) {
+                        Common::echo_log("Admin: 参数不足，退出");
+                        return true;
+                    }
+
+                    $value = $tmp[1];
+
                     //设置值
                     for ($set = strtok($value, ", \n\t"); $set !== false; $set = strtok(" \n\t")) {
                         Common::echo_log("Admin: 处理参数:{$set}");
@@ -144,6 +146,13 @@ class Admin extends Base
                 }
             //删除管理员
             case 'rm':{
+                    if (count($tmp) != 2) {
+                        Common::echo_log("Admin: 参数不足，退出");
+                        return true;
+                    }
+
+                    $value = $tmp[1];
+
                     //设置值
                     for ($set = strtok($value, ", \n\t"); $set !== false; $set = strtok(" \n\t")) {
                         Common::echo_log("Admin: 处理参数:{$set}");
@@ -160,8 +169,8 @@ class Admin extends Base
                         $admin = json_decode($admin, true);
 
                         //如果是 总管理员添加的，群管理员不能删除
-                        if (in_array($admin['add_by_id'], $big_admins)) {
-                            if (!(in_array($this->from_id, $big_admins))) {
+                        if (in_array($admin['add_by_id'], $sup_admins)) {
+                            if (!(in_array($this->from_id, $sup_admins))) {
                                 $res_str .= "{$set}: can't remove, add by super admin: {$admin['add_by_name']}" . PHP_EOL;
                             }
                         }
@@ -175,6 +184,24 @@ class Admin extends Base
 
                         $set = strtok(" \n\t");
                     }
+
+                    break;
+                }
+            //查看管理员列表
+            case 'ls':{
+                    $res_str .= 'Super adminer:' . PHP_EOL;
+                    foreach ($sup_admins as $k => $v) {
+                        $user = $redis->hGet($bot . 'users:ids', $v);
+                        $res_str .= $user . PHP_EOL;
+                    }
+                    $res_str .= PHP_EOL;
+
+                    $res_str .= 'Chat adminer:' . PHP_EOL;
+                    foreach ($chat_admins as $k => $v) {
+                        $user = $redis->hGet($bot . 'users:ids', $k);
+                        $res_str .= $user . PHP_EOL;
+                    }
+                    $res_str .= PHP_EOL;
 
                     break;
                 }
